70164a677691641f258a92389a2e84ba39f48e40
cdrosales <cara10denise@hotmail.com>
2020-01-21 10:40:25 -0500

    chore: add photography page

--- themes/portfolioTheme/functions.php
+++ themes/portfolioTheme/functions.php
@@ -1,18 +1,18 @@
 <?php
 
 // Adds script and stylesheets
-function inhabitant_files(){
+function portfolio_files(){
     // NAME & LOCATION FOR PARAMETERS
-    wp_enqueue_style('inhabitant_styles', get_stylesheet_uri(), NULL, microtime()); // first parameter name of styleshee & second is the location
+    wp_enqueue_style('portfolio_styles', get_stylesheet_uri(), NULL, microtime()); // first parameter name of styleshee & second is the location
     wp_enqueue_style('fonts', 'https://fonts.googleapis.com/css?family=Lato&display=swap');
     wp_enqueue_style('font-awesome', "https://use.fontawesome.com/releases/v5.8.1/css/all.css"); 
 
 }
 
-add_action('wp_enqueue_scripts', 'inhabitant_files');//add stylesheet
+add_action('wp_enqueue_scripts', 'portfolio_files');//add stylesheet
 
 //Adds theme support - ex: title tag
-function inhabitant_features(){
+function portfolio_features(){
     add_theme_support('post-thumbnails');
     add_theme_support ('title-tag'); 
     register_nav_menus( array(
@@ -21,5 +21,30 @@ function inhabitant_features(){
         'shop' => 'Product Categories Menu'
     ));
 }
-add_action('after_setup_theme', 'inhabitant_features');
+add_action('after_setup_theme', 'portfolio_features');
+
+
+
+function portfolio_photography(){
+    register_post_type('photography', array( 
+        'supports' => array('title', 'editor', 'thumbnail'),
+        'public' => true,
+        'show_in_rest' => true,
+        'has_archive' => true,
+        'labels' => array(
+            'name' => 'Photography', 
+            'add_new_item' => 'Add New Photograph',
+            'edit_item' => 'Edit Photograph',
+            'all_items' => 'All Photography',
+            'singular_name' => 'Photograph' 
+        ),
+
+        'menu_icon' => 'dashicons-camera-alt'
+    ));
+}
+
+add_action('init', 'portfolio_photography');
+
+
+
 ?>
